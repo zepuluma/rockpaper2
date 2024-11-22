5a7eb3a1aa0079685b300a3b6af62bc96acc70e8
PumbaMBP <132657739+zepuluma@users.noreply.github.com>
2024-11-22 10:25:56 +0100

    erste änderung

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-/*
+
 console.log("bingli");
 
 let humanScore = 0;
@@ -88,65 +88,4 @@ playGame();  */
 const output = document.querySelector('.output');
 output.textContent = "";
  */
-
-// let i = 10;
-
-
-for (let z = 10; z >= 0; z--) {
-const para = document.createElement('p');
-
-      //conditional operator ?
-      //deutlich übersichtlicher als viele for-schleifen mit (z == 10) und geschwungenen klammern
-      https://javascript.info/ifelse#:~:text=The%20so%2Dcalled%20%E2%80%9Cconditional%E2%80%9D,JavaScript%20which%20has%20that%20many.
-    
-      z == 10 ? console.log("Countdown 10") : //erster eintrag in der schleife
-      z == 0 ? console.log("START!") : // letzter eintrag in schleife
-      console.log(z); // alles dazwischen
-      
-
-
-
-}    
-
-/* 
-if (z = 10) {
-  para.textContent = "countdown 10";
-  break;
-}
-else if (z=1) {
-  para.textContent = "start";
-  break;
-}
-else {
-para.textContent = "hallo";
-output.appendChild(para);
-break;
-} */
-
-const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
-
-
-const all = document.querySelector('.all');
-const admitted = document.querySelector('.admitted');
-const refused = document.querySelector('.refused');
-
-all.textContent = 'All: ' + people.toString();
-admitted.textContent = 'Admit: ';
-refused.textContent = 'Refuse: ';
-
-// loop starts here
-for (const person in people) {
-  if (people[person] == 'Lola' || people[person] == 'Phil' ) {
-    refused.textContent += people[person];
-    console.log(person);
-  } else {
-    admitted.textContent += people[person];    
-  }
-
-  
-
-
-}
-// 
-// admitted.textContent += ;
 
